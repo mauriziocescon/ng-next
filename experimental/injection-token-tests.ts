3dd99901c6cb8cec5944e47daa2c5c630abd485f
Maurizio Cescon <maurizio.cescon@gmail.com>
2026-05-14 13:38:03 +0200

    chore: minor changes

--- experimental/injection-token-tests.ts
+++ experimental/injection-token-tests.ts
@@ -1,17 +1,9 @@
-import { Component, inject, Injectable, InjectionToken } from '@angular/core';
+import { Component, inject, Injectable } from '@angular/core';
 import {
   provide,
   injectionToken,
   inject as newInject,
 } from '../types/ng-types';
-
-@Injectable()
-class Store {
-  x = 0;
-}
-
-// legacy
-const token = new InjectionToken<number>('multi');
 
 // new
 const t1 = injectionToken<number>();
@@ -23,27 +15,29 @@ const t2 = injectionToken<string>({
   multi: true,
 });
 
-const t3 = t2;
+const t3 = injectionToken<unknown>();
+
+@Injectable()
+class Store {
+  x = 0;
+}
 
 @Component({
   selector: 'App',
   providers: [
-    { provide: token, useFactory: () => 1, multi: true },
-    { provide: token, useFactory: () => 'oops', multi: true }, // ❌ no error — wrong type
-    { provide: token, useFactory: () => 2 }, // ❌ no error — forgot multi, throws at runtime
-    provide({ token: Store, factory: () => new Date() }),
-    Store,
     provide(t1),
     provide({ token: t1 , factory: ()=> 10 }),
     provide(t2),
     provide({ token: t2, factory: ()=> '10' }),
+    provide({ token: Store, factory: () => new Date() }),
+    Store,
   ],
   template: ``,
 })
 export class App {
-  values = inject(token); // inferred as number, actually number[] at runtime
   t1 = newInject(t1);
   t2 = newInject(t2);
+  t3 = <string>newInject(t3);
   store = newInject(Store);
 
   func() {
