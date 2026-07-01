d23d09e941509d7f19d2651608f1c40a07fbfff5
Maurizio Cescon <maurizio.cescon@gmail.com>
2026-07-01 14:57:41 +0200

    chore: improve accessibility and mobile visualization

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://mauriziocescon.github.io',
   base: '/ng-next',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
