8344334f06f50b9cff95b4ae179e8eeef3ddb89e
Maurizio Cescon <maurizio.cescon@gmail.com>
2026-07-01 12:52:47 +0200

    chore: github-page

--- playground/astro.config.mjs
+++ playground/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+  site: 'https://mauriziocescon.github.io',
+  base: '/ng-next',
   integrations: [mdx()],
   markdown: {
     shikiConfig: {
